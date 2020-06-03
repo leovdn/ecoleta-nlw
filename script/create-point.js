e0a3a8c3863f5ae411a008aad1712604ddcfcbea
leovdn <leo_vdn@hotmail.com>
2020-06-03 16:41:20 -0300

    Adicionada seleção e inserção dos itens no JS

--- script/create-point.js
+++ script/create-point.js
@@ -44,8 +44,28 @@ for(const item of itemsToCollect) {
     item.addEventListener('click', handleSelectedItem)
 }
 
+const collectedItems = document.querySelector('input[name="items"]');
+let selectedItems = [];
+
 function handleSelectedItem(event) {
     const itemLi = event.target;
     itemLi.classList.toggle("selected");
     const itemId = itemLi.dataset.id;
+
+    const alreadySelected = selectedItems.findIndex( item => {
+        const itemFound = item == itemId;
+        return itemFound;
+    });
+
+    if( alreadySelected >= 0 ) {
+        const filteredItems = selectedItems.filter( item => {
+            const itemIsDifferent = item != itemId;
+            return itemIsDifferent;
+        });
+        selectedItems = filteredItems;
+    } else {
+        selectedItems.push(itemId);
+    }
+
+    collectedItems.value = selectedItems;
 }
